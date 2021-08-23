c7f5736b4f9315273dde160a9fc34692f9aa5148
JoJo-designs <jodiladouceur.design@gmail.com>
2021-08-23 11:02:51 -0400

    start building functions that check answers

--- asset/multi.js
+++ asset/multi.js
@@ -1,10 +1,12 @@
-const startBtn = document.querySelector("#startBtn")
-const questionBox = document.querySelector("#questionBox")
+const startBtn = document.querySelector("#startBtn");
+const questionBox = document.querySelector("#questionBox");
 const question = document.querySelector("#question");
-var userAnswer = document.getElementById("userAnswer").value
-const submit = document.querySelector("#submit")
+var userAnswer = document.getElementById("userAnswer");
+const submit = document.querySelector("#submit");
 
 let answer;
+let correct = 0;
+let incorrect = 0;
 
 //  LIST OF WHAT I NEED TO BUILD TO MAKE THIS WORK
 //  - hide question box before the start button is clicked DONE
@@ -28,12 +30,24 @@ function getRandom() {
     let valueOne = Math.floor(Math.random() * 13)
     let valueTwo = Math.floor(Math.random() * 13)
     answer = valueOne*valueTwo
-    console.log(answer)
     question.textContent = `${valueOne} x ${valueTwo} = ?`
 }
 
 submit.addEventListener("click", function () {
-    console.log("clicked")
-    console.log(userAnswer)
+    var userResponse = document.getElementById("userAnswer")
+    const userAnswer = parseInt(userResponse.value)
+    checkAnswer(userAnswer)
 });
+
+function checkAnswer(userAnswer) {
+ console.log(userAnswer)
+ console.log(answer)
+ if ( userAnswer === answer) {
+     console.log("correct")
+     correct = correct + 1;
+     console.log(correct);
+ } else {
+     console.log("incorrect")
+ }
+}
 
